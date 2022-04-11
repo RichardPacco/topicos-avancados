0bd228282b9fcfcf685332e51e61fc533bc42235
RichardPacco <richard.pacco@live.com>
2022-04-10 22:12:10 -0300

    Ajuste

--- modulo-produto/src/main/java/senai/topicos/swagger/SwaggerConfig.java
+++ modulo-produto/src/main/java/senai/topicos/swagger/SwaggerConfig.java
@@ -2,17 +2,9 @@ package senai.topicos.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.google.common.collect.ImmutableList;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -21,29 +13,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig  {                                    
+public class SwaggerConfig {
     @Bean
-    public Docket api() { 
-     
-    	Docket docket =  new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("senai.topicos.controller"))
-        .paths(PathSelectors.any())
-        .build()
-        .useDefaultResponseMessages(false)
-        .apiInfo(apiInfo());
-        
-        
+    public Docket api() {
+
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("senai.topicos.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo());
+
+
         return docket;
     }
-    
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("API topicos avancados")
-                .version("1.0.0")                
+                .version("1.0.0")
                 .contact(new Contact("Richard Pacco", "", "richard.pacco@live.com"))
                 .build();
     }
-    
-   
+
+
 }
