e093774707c783e28cf138af5278f2d5efcb81ef
RichardPacco <richard.pacco@live.com>
2022-06-06 17:09:20 -0300

    teste

--- estoque/src/main/java/senai/topicos/estoque/EstoqueApplication.java
+++ estoque/src/main/java/senai/topicos/estoque/EstoqueApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableBatchProcessing
 public class EstoqueApplication {
-//TODO subir aplicação para nuvem, heroku
+//TODO subir aplicação para nuvem, heroku blablabal
     public static void main(String[] args) {
         SpringApplication.run(EstoqueApplication.class, args);
     }
